445e785884f55c64ec0afe6da58b82fcb223537b
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2022-03-21 16:51:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.post('/post', express.urlencoded({ extended: true }), express.json(), (req, 
   
   const sender = req.headers['amp-email-sender'];
   console.log('Sender: ' + sender);
-  
+
+  // check if the sender is the right person (if the user is legitimate and is the one who's meant to use this api (to receive the email))
+  // if not, block the request
   if(!sender) {
     return res.status(403).send('This endpoint is not meant for you.');
   }

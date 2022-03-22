b27c6c8782c9c72abcf45aba1df1883edf1b66de
Krishna Moorthy <akrishnamoorthy007@gmail.com>
2022-03-22 15:44:39 +0530

    added delivery tracker endpoint

--- index.js
+++ index.js
@@ -12,16 +12,16 @@ app.use(cors());
 
 app.post('/post', express.urlencoded({ extended: true }), express.json(), (req, res) => {
   const user = req.body;
-  
+
   const sender = req.headers['amp-email-sender'];
   console.log('Sender: ' + sender);
 
   // check if the sender is the right person (if the user is legitimate and is the one who's meant to use this api (to receive the email))
   // if not, block the request
-  if(!sender) {
+  if (!sender) {
     return res.status(403).send('This endpoint is not meant for you.');
   }
-  
+
   res.setHeader('AMP-Email-Allow-Sender', sender);
 
   axios
@@ -34,6 +34,29 @@ app.post('/post', express.urlencoded({ extended: true }), express.json(), (req, 
   console.log(user);
 });
 
-const server = app.listen(PORT || 3000, () => {
+app.get('/track', (req, res) => {
+  const { trackingId } = req.query;
+
+  const trackingDetails = {
+    trackingId,
+    orderedOn: new Date('03-15-2022'),
+    currentState: 'Out for delivery',
+    lastUpdated: new Date('03-21-2022'),
+    eta: new Date('03-22-2022'),
+  };
+
+  const sender = req.headers['amp-email-sender'];
+  console.log(`Sender: ${sender}`);
+
+  if (!sender) {
+    return res.status(403).send('This endpoint is not meant for you.');
+  }
+
+  res.setHeader('AMP-Email-Allow-Sender', sender);
+
+  return res.send({ items: [trackingDetails] });
+});
+
+const server = app.listen(PORT || 4000, () => {
   console.log(`Server is listening at port ${server.address().port}`);
 });

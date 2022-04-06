b384f261bc6911c2a4242157a73bbe3d970a31a3
KrishnaMoorthy12 <akrishnamoorthy007@gmail.com>
2022-04-06 17:33:07 +0530

    added form submission check endpoint

--- forms.js
+++ forms.js
@@ -30,14 +30,31 @@ router.post(
   }
 );
 
+router.get(
+  '/is-survey-submitted',
+  express.urlencoded({ extended: true }),
+  express.json(),
+  protectRoute,
+  async (req, res) => {
+    const submittedUsers = await axios.get(`https://${DB_NAME}.restdb.io/rest/survey`, {
+      headers: { 'x-apikey': API_KEY },
+    });
+
+    if (submittedUsers.indexOf(req.body.userId) < 0)
+      return res.status(200).json({ message: 'User has not submitted the survey' });
+
+    return res.status(400).json({ message: 'User has already submitted the form' });
+  }
+);
+
 router.post(
   '/survey',
   express.json(),
   express.urlencoded({ extended: true }),
   protectRoute,
   (req, res) => {
-    const { fullName, age, dateOfJoining } = req.body;
-    const survey = { fullName, age: +age, dateOfJoining: new Date(dateOfJoining) };
+    const { fullName, age, dateOfJoining, userId } = req.body;
+    const survey = { fullName, age: +age, dateOfJoining: new Date(dateOfJoining), userId };
     axios
       .post(`https://${DB_NAME}.restdb.io/rest/survey`, survey, {
         headers: { 'x-apikey': API_KEY },
